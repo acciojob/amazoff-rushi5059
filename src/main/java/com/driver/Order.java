c46ee51ad0c005cbef1db82b52166b9c683ad711
Rushikesh Pote <rushi5059@gmail.com>
2023-05-27 18:11:24 +0530

    service done

--- src/main/java/com/driver/Order.java
+++ src/main/java/com/driver/Order.java
@@ -6,16 +6,12 @@ public class Order {
     private int deliveryTime;
 
     public Order(String id, String deliveryTime) {
-
+        this.id = id;
         // The deliveryTime has to converted from string to int and then stored in the attribute
         //deliveryTime  = HH*60 + MM
-        this.id=id;
-
-        String arr[]=deliveryTime.split(":");//12:45
-        int hr=Integer.parseInt(arr[0]);
-        int min=Integer.parseInt(arr[1]);
-
-        this.deliveryTime=(hr*60+min);
+        String [] sep = deliveryTime.split(":");
+        int time = (Integer.parseInt(sep[0])*60)+ (Integer.parseInt(sep[1]));
+        this.deliveryTime = time;
     }
 
     public String getId() {
--- src/main/java/com/driver/OrderService.java
+++ src/main/java/com/driver/OrderService.java
@@ -1,70 +1,68 @@
-
 package com.driver;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import java.util.ArrayList;
 import java.util.List;
-import java.util.Map;
+
 @Service
 public class OrderService {
-    OrderRepository orderRepository=new OrderRepository();
+
+    @Autowired
+    OrderRepository Orderepo;
 
 
     public void addOrder(Order order) {
-        orderRepository.addOrder(order);
+        Orderepo.addOrder(order);
     }
-
 
     public void addPartner(String partnerId) {
-        orderRepository.addPartner(partnerId);
+        Orderepo.addPartner(partnerId);
     }
 
-
     public void addOrderPartnerPair(String orderId, String partnerId) {
-        orderRepository.addOrderPartnerPair(orderId,partnerId);
+        Orderepo.addOrderPartnerPair(orderId,partnerId);
     }
 
     public Order getOrderById(String orderId) {
-        return orderRepository.getOrderById(orderId);
+        return Orderepo.getOrderByID(orderId);
     }
 
     public DeliveryPartner getPartnerById(String partnerId) {
-        return orderRepository.getPartnerById(partnerId);
+        return Orderepo.getPartnerById(partnerId);
     }
 
     public Integer getOrderCountByPartnerId(String partnerId) {
-        return orderRepository.getOrderCountByPartnerId(partnerId);
+        return Orderepo.getOrderCountByPartnerId(partnerId);
     }
 
     public List<String> getOrdersByPartnerId(String partnerId) {
-        return orderRepository.getOrdersByPartnerId(partnerId);
+        return Orderepo.getOrdersByPartnerId(partnerId);
     }
 
-    public List<String> getAllOrders() {
-        return orderRepository.getAllOrders();
+    public List<String> getAllOrders(List<String> orders) {
+        return Orderepo.getAllOrders(orders);
     }
 
     public Integer getCountOfUnassignedOrders() {
-        return orderRepository.getCountOfUnassignedOrders();
+        return Orderepo.getCountOfUnassignedOrders();
     }
 
     public Integer getOrdersLeftAfterGivenTimeByPartnerId(String time, String partnerId) {
-        return orderRepository.getOrdersLeftAfterGivenTimeByPartnerId(time,partnerId);
+        // should convert time here  string to int
+        return Orderepo.getOrdersLeftAfterGivenTimeByPartnerId(time, partnerId);
     }
 
     public String getLastDeliveryTimeByPartnerId(String partnerId) {
-        return orderRepository.getLastDeliveryTimeByPartnerId(partnerId);
+        // return int to string which is done in repo only
+        return Orderepo.getLastDeliveryTimeByPartnerId(partnerId);
     }
 
-    public void deletePartnerById(String partnerId) {
-        orderRepository.deletePartnerById(partnerId);
+    public void deletePartnerId(String partnerId) {
+        Orderepo.deletePartnerId(partnerId);
     }
 
     public void deleteOrderById(String orderId) {
-        orderRepository.deleteOrderById(orderId);
+        Orderepo.deleteOrderById(orderId);
     }
-
-
 }
